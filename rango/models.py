50c27cbeb2b8f4c029e26ce847b3a2d07874448e
Eve O'Hagan <2251923o@student.gla.ac.uk>
2018-01-25 18:07:08 +0000

    end of 5

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
     def __str__(self): # For Python 2, use __unicode__ too
         return self.name
 
@@ -14,3 +17,7 @@ class Page(models.Model):
 
     def __str__(self): # For Python 2, use __unicode__ too
         return self.title
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+    
