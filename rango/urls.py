973e7f6a9c0a84ef26f938b09eeea17dd9af4838
Eve O'Hagan <2251923o@student.gla.ac.uk>
2018-01-26 14:56:54 +0000

    Includes exercise in chapter 4

--- rango/urls.py
+++ rango/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
+from django.contrib import admin
+from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
 url(r'^$', views.index, name='index'),
-]
+url(r'^$', admin.site.urls),
+url(r'$', views.about, name ='about'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
