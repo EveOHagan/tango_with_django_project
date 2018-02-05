df74a89fa8d994f4a51c26d3b1b49a67ea600d3f
Eve O'Hagan <2251923o@student.gla.ac.uk>
2018-02-05 14:00:18 +0000

    chapter 8.2

--- rango/forms.py
+++ rango/forms.py
@@ -4,8 +4,9 @@ from rango.models import Page, Category
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
                            help_text="Please enter the category name.")
-    help_text="Please enter the category name.")
-    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
 
     class Meta:
@@ -17,9 +18,19 @@ class PageForm(forms.ModelForm):
                             help_text="Please enter the title of the page.")
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
-    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
 
     
     class Meta:
         model = Page
-        exclude = ('category',)
+        exclude = ('category',)
+
